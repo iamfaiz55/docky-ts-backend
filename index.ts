56fa20b29d3216ecd7120968d96997b87293a507
shaikh faiz <138873480+iamfaiz55@users.noreply.github.com>
2024-12-05 17:13:58 +0530

    frontend link added

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://docky-ts.vercel.app",
+    origin: "https://docky-ts-frontend.vercel.app",
     // origin: "http://localhost:5173",
     credentials: true,
 }));
